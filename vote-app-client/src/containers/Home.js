1d2524cb76e385bf41480ecbec0235cf147237cb
Vinay Krisha <vinaykrishna1989@gmail.com>
2018-10-02 16:17:19 +0530

    added routes, Home page and NotFound page

---
+++ vote-app-client/src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div className="Home">
+                <div className="lander">
+                    <h1>Vote</h1>
+                    <p>Voting app to select your Public Servent</p>
+                </div>
+            </div>
+        );
+    }
+}
